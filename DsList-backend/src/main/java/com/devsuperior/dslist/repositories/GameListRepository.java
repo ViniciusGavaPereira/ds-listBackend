e4724750dc0b729d98aed77e1869e2515c1a0d8b
Vinicius <vini-gava@outlook.com.br>
2023-07-13 23:12:34 -0300

    aula 4

--- DsList-backend/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ DsList-backend/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long>{
     
+    @Modifying
+    @Query(nativeQuery = true, 
+     value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(@Param("listId") Long listId, @Param("gameId") Long gameId, @Param("newPosition") Integer newPosition);
 }
